699a76d67252fd639c914b30742ecf159dfb6763
efigueira-voxsmart <elena.figueira@voxsmart.com>
2024-12-02 12:40:20 +0100

    Cosmetic change

--- main.py
+++ main.py
@@ -29,6 +29,7 @@ def go(config: DictConfig):
         _ = mlflow.run(
             os.path.join(root_path, "download"),
             "main",
+            # Los parámetros del file_url están definidos en el archivo config.yaml
             parameters={
                 "file_url": config["data"]["file_url"],
                 "artifact_name": "raw_data.parquet",
@@ -39,34 +40,85 @@ def go(config: DictConfig):
 
     if "preprocess" in steps_to_execute:
 
-        ## YOUR CODE HERE: call the preprocess step
-        pass
+        _ = mlflow.run(
+            os.path.join(root_path, "preprocess"),
+            "main",
+            parameters={
+                # El input es la salida del download
+                "input_artifact": "raw_data.parquet",
+                "artifact_name": "processed_data.csv",
+                "artifact_type": "processed_data",
+                "artifact_description": "Data with preprocessing applied"
+            },
+        )
 
     if "check_data" in steps_to_execute:
 
-        ## YOUR CODE HERE: call the check_data step
-        pass
+        _ = mlflow.run(
+            os.path.join(root_path, "check_data"),
+            "main",
+            parameters={
+                # config.yaml
+                "reference_artifact": config["data"]["reference_dataset"],
+                # El input es la salida del download
+                "sample_artifact": "processed_data.csv:latest",
+                # config.yaml
+                "ks_alpha": config["data"]["ks_alpha"],
+            },
+        )
 
     if "segregate" in steps_to_execute:
 
-        ## YOUR CODE HERE: call the segregate step
-        pass
+        _ = mlflow.run(
+            os.path.join(root_path, "segregate"),
+            "main",
+            parameters={
+                # El input es la salida del check_data
+                "input_artifact": "preprocessed_data.csv:latest",
+                # Name for the W&B artifact that will be created
+                # This step will provide 2 artifacts: train_{artifact_root}.csv and test_{artifact_root}.csv
+                "artifact_root": "data",
+                "artifact_type": "segregated_data",
+                # config.yaml
+                "test_size": config["data"]["test_size"],
+                "random_state": config["data"]["random_state"],
+                "stratify": config["data"]["stratify"],
+            },
+        )
 
     if "random_forest" in steps_to_execute:
 
-        # Serialize decision tree configuration
+        # Para no tener que cargar todos los parámetros del random forest uno a uno, hacemos esto
         model_config = os.path.abspath("random_forest_config.yml")
 
         with open(model_config, "w+") as fp:
             fp.write(OmegaConf.to_yaml(config["random_forest_pipeline"]))
 
-        ## YOUR CODE HERE: call the random_forest step
-        pass
+        _ = mlflow.run(
+            os.path.join(root_path, "random_forest"),
+            "main",
+            parameters={
+                # El input es la salida del segregate
+                "train_data": "data_train.csv:latest",
+                "model_config": model_config,
+                "export_artifact": config["random_forest_pipeline"]["export_artifact"],
+                "random_seed": config["main"]["random_seed"],
+                "val_size": config["data"]["test_size"],
+                "stratify": config["data"]["stratify"]
+            },
+        )
 
     if "evaluate" in steps_to_execute:
 
-        ## YOUR CODE HERE: call the evaluate step
-        pass
+        _ = mlflow.run(
+            os.path.join(root_path, "evaluate"),
+            "main",
+            parameters={
+                # El input es la salida del random_forest
+                "model_export": f"{config['random_forest_pipeline']['export_artifact']}:latest",
+                "test_data": "data_test.csv:latest"
+            },
+        )
 
 
 if __name__ == "__main__":
